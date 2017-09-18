aa89e2fd7d6575f01fac78bb3a7ae0771279c120
shilpa1216 <shilpa1216@gmail.com>
2017-09-18 15:12:04 -0500

    map practice

--- Chapter 15 Activities/Gradebook/Gradebook.java
+++ Chapter 15 Activities/Gradebook/Gradebook.java
@@ -1,49 +1,70 @@
 package Gradebook;
 
 import java.util.Scanner;
-. . .
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 /**
-   A program to add, remove, modify or print
-   student names and grades.
-*/
+A program to add, remove, modify or print
+student names and grades.
+ */
 public class Gradebook
 {
-   public static void main(String[] args)
-   {
-      Scanner in = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
 
-      . . .
-
-      boolean done = false;
-      while(!done)
-      {
-         System.out.println("A)dd R)emove M)odify P)rint Q)uit");
-         String input = in.next().toUpperCase();
-         if (input.equals("Q"))
-         {
-            done = true;
-         }
-         else if (input.equals("A"))
-         {
-            . . .
-
-         }
-         else if (input.equals("R"))
-         {
-            . . .
-         }
-         else if (input.equals("M"))
-         {
-            . . .
-         }
-         else if (input.equalsIgnoreCase("P"))
-         {
-            . . .
-         }
-         else
-         {
-            done = true;
-         }
-      }
-   }
+        Map<String,String > grades = new HashMap<>();
+        Set<String> keySet = grades.keySet();
+        
+        boolean done = false;
+        while(!done)
+        {
+            System.out.println("A)dd R)emove M)odify P)rint Q)uit");
+            String input = in.next().toUpperCase();
+            if (input.equals("Q"))
+            {
+                done = true;
+            }
+            else if (input.equals("A"))
+            {
+                System.out.println("Name of person to add");
+                String name = in.next().toUpperCase();
+                System.out.println("What is their grade?");
+                String grade = in.next().toUpperCase();
+                grades.put(name, grade);
+            }
+            else if (input.equals("R"))
+            {
+                System.out.println("Name of person to remove");
+                String rem_name = in.next().toUpperCase();
+                grades.remove(rem_name);
+            }
+            else if (input.equals("M"))
+            {
+                System.out.println("Name of person to modify");
+                String mod_name = in.next().toUpperCase();
+                for (String key: keySet)
+                {
+                    if (key.equals(mod_name))
+                    {
+                        System.out.println("Grade:");
+                        String mod_grade = in.next().toUpperCase();
+                        grades.put(mod_name, mod_grade);
+                    }
+                }
+            }
+            else if (input.equalsIgnoreCase("P"))
+            {
+                for (String key: keySet)
+                {
+                    System.out.println( key + ": " + grades.get( key )); 
+                }
+            }
+            else
+            {
+                done = true;
+            }
+        }
+    }
 }
